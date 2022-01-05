460020da100c383de2f9ed14174b0a94e18bf0ac
Felix Wong <felixwongyuenlam@gmail.com>
2022-01-05 17:59:11 +0800

    Before start of file upload section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,24 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+      >
+        Music
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"
+              >About
+            </router-link>
+          </li>
+
           <li v-if="userLoggedIn === false">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
@@ -20,7 +32,9 @@
               >
             </li>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }"
+                >Manage</router-link
+              >
             </li>
           </template>
         </ul>
@@ -30,14 +44,23 @@
 </template>
 
 <script>
-import { mapMutations, mapState, mapActions } from "vuex";
+import { mapMutations, mapState } from "vuex";
 
 export default {
   name: "Header",
   methods: {
     //mapMutations return an Object
     ...mapMutations(["toggleAuthModal"]),
-    ...mapActions(["signout"]),
+
+    signout() {
+      this.$store.dispatch("signout", {
+        router: this.$router,
+      });
+
+      if (this.$route.meta.requiresAuth) {
+        this.$router.push({ name: "home" });
+      }
+    },
   },
   computed: {
     ...mapState(["userLoggedIn"]),
